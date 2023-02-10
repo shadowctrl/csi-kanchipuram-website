b27a614578c4e7ddbe8bee1a94a079aa2f2f1be6
shadowctrl <shadowctrl@outlook.com>
2023-02-10 18:07:00 +0530

    typewriter

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../../../constants";
 import content from "./content";
+import Typewriter from "typewriter-effect";
 
 const main = () => {
   return (
     <motion.div className={`py-28 px-24`}>
       <div>
         <h1 className={` text-slate-300 font-poppins font-extrabold text-2xl`}>
-          Welcome To CSI - Kancheepuram Chapter
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Welcome To CSI - Kancheepuram Chapter")
+                .start();
+            }}
+          />
         </h1>
         <p className=" font-extralight font-poppins text-slate-300 py-10">
-          {" "}
-          {content.welcome}
+          {`${content.welcome}`}
         </p>
       </div>
     </motion.div>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,15 +2,28 @@ import React, { useEffect } from "react";
 import navcon from "../../../constants/navbar";
 import "./navbar.css";
 import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+
 const navbar = () => {
   useEffect(() => {
     document.title = "CSI - Kancheepuram Chapter";
   });
   return (
-    <motion.div className="w-full flex justify-between items-center">
-      {console.log()}
-      <a href="/" className="p-10 ">
-        <img src={`/images/logo.png`} className="w-[80%] rounded-full"></img>
+    <motion.div className="w-full  justify-between items-center">
+      <a href="/" className="flex">
+        <img
+          src={`/images/logo.png`}
+          className="p-10 w-[15%] rounded-full"
+        ></img>
+
+        <h1 className="py-20 text-2xl font-poppins font-extrabold text-white">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString(`COMPUTER SOCEITY OF INDIA`);
+              typewriter.start();
+            }}
+          />
+        </h1>
       </a>
 
       <ul className="list-none flex justify-end items-center rounded-2xl">
