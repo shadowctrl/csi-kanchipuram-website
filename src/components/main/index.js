90c8e1e2295ff144aa24e4d287003377b434da71
shadowctrl <shadowctrl@outlook.com>
2023-02-16 06:01:25 +0530

    New Export

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,3 +1,4 @@
 export { default as Welcome } from "./welcome";
 export { default as Vision } from "./vision";
 export { default as Mission } from "./mission";
+export { default as Events } from "./events";
